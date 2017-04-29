25eae9f71d3b7a6b5b46d6a9b5a67a2c40d8a5d0
NeverMore27 <uragan.uragan.59@mail.ru>
2017-04-30 00:46:38 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -4,7 +4,7 @@ SCENARIO("NULL")
 {
 	int a[10] = { 1, 5, 6, 9 ,7, 2, 0 ,4, 5, 7 };
 	int b[10] = { 9, 7, 7, 6, 5 ,5,  4, 2, 1, 0 };
-	quickSort(&a[0], &a[10]);
+	quickSort(&a[0], &a[9]);
 	for (int i=0; i < 10; i++)
 	{
 		REQUIRE(a[i] == b[i]);
@@ -38,7 +38,7 @@ SCENARIO("NULL2")
 	a[0][0] = 1; a[0][1] = 2; a[0][2] = 3;
 	a[1][0] = 4; a[1][1] = 5; a[1][2] = 6;
 	a[2][0] = 7; a[2][1] = 8; a[2][2] = 9;		
-	quickSort(&a[0][0], &a[3][2]);
+	quickSort(&a[0][0], &a[2][2]);
 	REQUIRE(a[0][0] == 9); 
 	REQUIRE(a[0][1] == 8);
 	REQUIRE(a[0][2] == 7);
